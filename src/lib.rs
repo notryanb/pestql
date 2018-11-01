22fbee443771745c81382bbb6f02ca35c551a50f
Ryan Blecher <notryanb@gmail.com>
2018-11-01 13:27:42 -0400

    Add a few tests for basic queries

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,47 @@
+extern crate pest;
+#[macro_use] extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "sql.pest"]
+pub struct SQLParser;
+
+pub fn is_valid_sql(query: &str) -> bool {
+    SQLParser::parse(Rule::sql, query).is_ok()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn parses_simple_select() {
+        let statement = "select 1 from test;";
+        assert_eq!(true, is_valid_sql(statement))
+    }
+
+    #[test]
+    fn parses_select_star() {
+        let statement = "select * from test;";
+        assert_eq!(true, is_valid_sql(statement))
+    }
+
+    #[test]
+    fn parses_select_two_columns() {
+        let statement = "select something,another_one from test;";
+        assert_eq!(true, is_valid_sql(statement))
+    }
+
+    #[test]
+    fn parses_select_many_columns() {
+        let statement = "select something,another_one,yet_another,so_many_columns from test;";
+        assert_eq!(true, is_valid_sql(statement))
+    }
+
+        #[test]
+    fn error_when_trailing_comma_in_select() {
+        let statement = "select something,another_one,yet_another,so_many_columns, from test;";
+        assert_eq!(false, is_valid_sql(statement))
     }
 }
